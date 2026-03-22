490d01a19c7c7706f4162739c800d113e18719b3
Brice FROMENTIN <brice-fromentin@users.noreply.github.com>
2026-03-22 14:10:45 +0100

    chore: Remove old files to resolve PR merge conflict

--- src/Askyl.Dsm.WebHosting.Data/WebSites/IReverseProxyManager.cs
+++
@@ -1,9 +0,0 @@
-
-namespace Askyl.Dsm.WebHosting.Data.WebSites;
-
-public interface IReverseProxyManager
-{
-    Task CreateAsync(WebSiteConfiguration site);
-    Task UpdateAsync(WebSiteConfiguration site);
-    Task DeleteAsync(WebSiteConfiguration site);
-}
