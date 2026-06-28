b7bab58ebdae92dbf14e98bf9178b53f25d57f33
Brice FROMENTIN <brice-fromentin@users.noreply.github.com>
2026-06-28 12:26:57 +0200

    fix: cache HttpClient in AuthenticationService to prevent socket exhaustion

--- src/Askyl.Dsm.WebHosting.Ui.Client/Services/AuthenticationService.cs
+++ src/Askyl.Dsm.WebHosting.Ui.Client/Services/AuthenticationService.cs
@@ -22,10 +22,12 @@ namespace Askyl.Dsm.WebHosting.Ui.Client.Services;
 /// <param name="localizer">Localizer for user-facing strings.</param>
 public class AuthenticationService(IHttpClientFactory httpClientFactory, ILocalizer localizer) : IAuthenticationService
 {
+    private readonly HttpClient _httpClient = httpClientFactory.CreateClient(ApplicationConstants.HttpClientName);
+
     /// <inheritdoc/>
     public async Task<AuthenticationResult> LoginAsync(string login, string password, string? otpCode)
     {
-        var httpClient = httpClientFactory.CreateClient(ApplicationConstants.HttpClientName);
+        var httpClient = _httpClient;
 
         var jsonContent = new StringContent(JsonSerializer.Serialize(new LoginCredentials(login, password, otpCode), JsonOptionsCache.Options), Encoding.UTF8, NetworkConstants.ApplicationJson);
 
@@ -51,14 +53,14 @@ public class AuthenticationService(IHttpClientFactory httpClientFactory, ILocali
     /// <inheritdoc/>
     public async Task<ApiResult> LogoutAsync()
     {
-        var httpClient = httpClientFactory.CreateClient(ApplicationConstants.HttpClientName);
+        var httpClient = _httpClient;
         return await httpClient.PostJsonOrDefaultAsync<object, ApiResult>(AuthenticationRoutes.LogoutFullRoute, null, () => ApiResult.CreateFailure(localizer[LK.Error.Unknown]));
     }
 
     /// <inheritdoc/>
     public async Task<ApiResultBool> IsAuthenticatedAsync()
     {
-        var httpClient = httpClientFactory.CreateClient(ApplicationConstants.HttpClientName);
+        var httpClient = _httpClient;
         return await httpClient.GetJsonOrDefaultAsync(AuthenticationRoutes.StatusFullRoute, () => ApiResultBool.CreateFailure(localizer[LK.Error.FailedToCheckAuthStatus]));
     }
 
