bf50719b978f04424dfb7018df0ebffa27e818db
Brice FROMENTIN <brice-fromentin@users.noreply.github.com>
2026-06-29 17:51:58 +0200

    Revert "fix: disable test collection parallelization to prevent timeouts"
    This reverts commit f895e98a2c0af50d624ee56db6a29c624b229baf.

--- src/Askyl.Dsm.WebHosting.Tests/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using Xunit;
-
-// Run test collections sequentially to avoid resource contention between
-// CPU-heavy Roslyn analyzer tests and disk-heavy file I/O tests.
-[assembly: CollectionBehavior(DisableTestParallelization = true)]
